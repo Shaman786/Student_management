2e670a30e16e0895c6ba4149cd517e9bd5df0007
Fayaj Hossain <fayazhossain88@gmail.com>
2024-08-06 23:07:21 +0530

    chore: Remove unused code for student and subject management

--- src/main/java/com/shaman/fayaj/student_management/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shaman.fayaj.student_management.repository;
-
-import com.shaman.fayaj.student_management.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
--- src/main/java/com/shaman/fayaj/student_management/repository/SubjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shaman.fayaj.student_management.repository;
-
-import com.shaman.fayaj.student_management.entity.Subject;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubjectRepository extends JpaRepository<Subject, Long> {
-}
